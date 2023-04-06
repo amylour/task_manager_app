031401ceb55927a13711918f2ac4fb99a967aad4
Amy Richardson <amyloumc@gmail.com>
2023-04-06 17:27:51 +0000

    Add edit task route

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -15,7 +15,7 @@ def categories():
     return render_template("categories.html", categories=categories)
 
 
-@app.route("/add category", methods=["GET", "POST"])
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
@@ -25,7 +25,7 @@ def add_category():
     return render_template("add_category.html")
 
 
-@app.route("/edit.category/<int:category_id>", methods=["GET", "POST"])
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
     category = Category.query.get_or_404(category_id)
     if request.method == "POST":
@@ -33,6 +33,14 @@ def edit_category(category_id):
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
 
 
 @app.route("/add_task", methods=["GET", "POST"])
@@ -50,3 +58,17 @@ def add_task():
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("add_task.html", categories=categories)
+
+
+@app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task.task_name = request.form.get("task_name")
+        task.task_description = request.form.get("task_description")
+        task.is_urgent = bool(True if request.form.get("is_urgent") else False)
+        task.due_date = request.form.get("due_date")
+        task.category_id = request.form.get("category_id")
+        db.session.commit()
+    return render_template("edit_task.html", task=task, categories=categories)
